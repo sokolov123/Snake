62a49d7e54538ebc63f50ded152e3c7e371de03e
sokolov123 <anton2003zxcegth@gmail.com>
2021-05-21 12:45:43 +0300

    отредачил.

--- src/test/java/view/EatAppleTest.java
+++ src/test/java/view/EatAppleTest.java
@@ -18,22 +18,29 @@ class EatAppleTest {
         int score = 0;
         EatApple eatApple = new EatApple(s, apple, length, score);
         eatApple.eatApple();
-        eatApple.scorePlus();
         assertEquals(101,eatApple.getLength());
-        assertEquals(1, eatApple.getScore());
         eatApple.eatApple();
-        eatApple.scorePlus();
         assertEquals(102,eatApple.getLength());
-        assertEquals(2, eatApple.getScore());
         eatApple.eatApple();
-        eatApple.scorePlus();
         assertEquals(103,eatApple.getLength());
-        assertEquals(3, eatApple.getScore());
         eatApple.eatApple();
+        assertEquals(104,eatApple.getLength());
+        assertEquals(104,eatApple.getLength());
+    }
+
+    @Test
+    void Score() {
+        int length=100;
+        int score = 0;
+        EatApple eatApple = new EatApple(s, apple, length, score);
         eatApple.scorePlus();
-        assertEquals(104,eatApple.getLength());
+        assertEquals(1, eatApple.getScore());
+        eatApple.scorePlus();
+        assertEquals(2, eatApple.getScore());
+        eatApple.scorePlus();
+        assertEquals(3, eatApple.getScore());
+        eatApple.scorePlus();
         assertEquals(4, eatApple.getScore());
-        assertEquals(104,eatApple.getLength());
         assertEquals(4, eatApple.getScore());
     }
 }
